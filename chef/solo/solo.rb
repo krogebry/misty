7396f1434a8240f0970dda2fcd7d6ed5a449c76c
Ubuntu <ubuntu@chef-server0.ksonsoftware.com>
2012-08-11 07:42:02 +0000

    Updating scripts.

--- chef/solo/solo.rb
+++ chef/solo/solo.rb
@@ -1,6 +1,6 @@
 ##
 # Solo file.
-FS_ROOT = File.dirname( __FILE__ )
+FS_ROOT = File.expand_path(File.dirname( __FILE__ ))
 
 log_level :debug
 log_location STDOUT
@@ -8,10 +8,5 @@ cache_type "BasicFile"
 cache_options({ :path => "%s/.chef/checksums" % ENV['HOME'], :skip_expires => true })
 file_cache_path "%s/.chef/cache/" % ENV['HOME']
 file_backup_path "%s/.chef/cache/" % ENV['HOME']
-#cookbook_path "~/var/chef-solo/cookbooks"
 role_path "%s/roles" % FS_ROOT
 cookbook_path "%s/cookbooks" % FS_ROOT
-#cookbook_path "/home/krogebry/Dropbox/Private/Krogebry/chef/cookbooks"
-#role_path "/home/krogebry/Dropbox/Private/Krogebry/chef/roles"
-
-
